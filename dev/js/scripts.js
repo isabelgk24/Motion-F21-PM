5956de78712b3ff0d910aca07a1e138750c49f5b
isabelgk24 <igk23@drexel.edu>
2021-10-03 19:41:18 -0400

    Hero animated

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,7 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+const mainTL = gsap.timeline();
+
+mainTL.from("#hero",{duration:2,alpha:0})
+    .from("#hero h1", {duration:1, y:"+=200", alpha:0})
+    .from("#hero i", {duration:1, y:"-=200", alpha:0, rotation:180}), "50%";
