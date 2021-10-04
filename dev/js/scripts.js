837437030227d84f182a75256f6797321d28afed
isabelgk24 <igk23@drexel.edu>
2021-10-03 20:08:07 -0400

    Done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,15 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero",{duration:2,alpha:0})
-    .from("#hero h1", {duration:1, y:"+=200", alpha:0})
-    .from("#hero i", {duration:1, y:"-=200", alpha:0, rotation:180}), "50%";
+// const heroHeight = document.querySelector("#hero");
+
+const aniTime = 0.75;
+
+// console.log(heroHeight.clientHeight + "px is height of hero section");
+// console.log(heroHeight.clientWidth + "px is width of hero section");
+
+mainTL.from("#hero",{duration:2, alpha:0})
+    .from("#hero h1", {duration:aniTime, y:"+=200", alpha:0})
+    .from("#hero i", {duration:aniTime, y:"-=200", alpha:0, rotation:180}), "50%"
+
+mainTL.from("#content1 ul li i",{duration:aniTime, y:"+=10", alpha:0})
