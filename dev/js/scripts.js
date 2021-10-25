d716f0d5e5fa99ef14559452007f9a27bb98b1d1
isabelgk24 <igk23@drexel.edu>
2021-10-25 09:06:16 -0400

    tmeline set up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,15 +2,91 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-// const heroHeight = document.querySelector("#hero");
+function grassMorph(){
+    const tl =gsap.timeline();
+    tl.to("#Grass",{duration:1, scale:2})
+    return tl;
+}
 
-const aniTime = 0.75;
+function mountainSlide1(){
+    const tl =gsap.timeline();
+    tl.to("#mountain1",{duration:1, scale:2})
+    return tl;
+}
 
-// console.log(heroHeight.clientHeight + "px is height of hero section");
-// console.log(heroHeight.clientWidth + "px is width of hero section");
+function mountainSlide2(){
+    const tl =gsap.timeline();
+    tl.to("#mountain2",{duration:1, scale:2})
+    return tl;
+}
 
-mainTL.from("#hero",{duration:2, alpha:0})
-    .from("#hero h1", {duration:aniTime, y:"+=200", alpha:0})
-    .from("#hero i", {duration:aniTime, y:"-=200", alpha:0, rotation:180}), "50%"
+function mountainSlide3(){
+    const tl =gsap.timeline();
+    tl.to("#mountain3",{duration:1, scale:2})
+    return tl;
+}
 
-mainTL.from("#content1 ul li i",{duration:aniTime, y:"+=10", alpha:0})
+mainTL.add(grassMorph())
+        .add(mountainSlide1())
+        .add(mountainSlide2())
+        .add(mountainSlide3());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const heroHeight = document.querySelector("#hero");
+
+// const aniTime = 0.75;
+
+// // console.log(heroHeight.clientHeight + "px is height of hero section");
+// // console.log(heroHeight.clientWidth + "px is width of hero section");
+
+// mainTL.from("#hero",{duration:2, alpha:0})
+//     .from("#hero h1", {duration:aniTime, y:"+=200", alpha:0})
+//     .from("#hero i", {duration:aniTime, y:"-=200", alpha:0, rotation:180}), "50%"
+
+// mainTL.from("#content1 ul li i",{duration:aniTime, y:"+=10", alpha:0})
