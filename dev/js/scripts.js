3c4877c7b7c9eba9b70c6bc98170d500eaaac295
isabelgk24 <igk23@drexel.edu>
2021-11-01 07:41:10 -0400

    road/mountains/lamps

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,38 +5,64 @@ gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
-function grassMorph(){
-    const tl =gsap.timeline();
-    tl.to("#Grass",{duration:1, scale:2})
+function appear(){
+    const tl = gsap.timeline();
+    tl.from("#Road",{duration: 0.5, y:"-=25", alpha:0});
+
     return tl;
 }
 
-function mountainSlide1(){
-    const tl =gsap.timeline();
-    tl.to("#mountain1",{duration:1, scale:2})
+function mountainSlide(){
+    const tl = gsap.timeline();
+    tl.from(".mountains",{duration: 0.5, y:"+=50", alpha:0, stagger: 0.25});
+
     return tl;
 }
 
-function mountainSlide2(){
-    const tl =gsap.timeline();
-    tl.to("#mountain2",{duration:1, scale:2})
+function lightAppear(){
+    const tl = gsap.timeline();
+    tl.from(".lamp",{duration: 1.5, alpha:0, stagger: 0.5});
+
     return tl;
 }
 
-function mountainSlide3(){
-    const tl =gsap.timeline();
-    tl.to("#mountain3",{duration:1, scale:2})
-    return tl;
-}
-
-mainTL.add(grassMorph())
-        .add(mountainSlide1())
-        .add(mountainSlide2())
-        .add(mountainSlide3());
+mainTL.add(appear())
+        .add(mountainSlide())
+        .add(lightAppear());
 
 
 
 
+
+
+// function grassMorph(){
+//     const tl =gsap.timeline();
+//     tl.to("#Grass",{duration:1, scale:2})
+//     return tl;
+// }
+
+// function mountainSlide1(){
+//     const tl =gsap.timeline();
+//     tl.to("#mountain1",{duration:1, scale:2})
+//     return tl;
+// }
+
+// function mountainSlide2(){
+//     const tl =gsap.timeline();
+//     tl.to("#mountain2",{duration:1, scale:2})
+//     return tl;
+// }
+
+// function mountainSlide3(){
+//     const tl =gsap.timeline();
+//     tl.to("#mountain3",{duration:1, scale:2})
+//     return tl;
+// }
+
+// mainTL.add(grassMorph())
+//         .add(mountainSlide1())
+//         .add(mountainSlide2())
+//         .add(mountainSlide3());
 
 
 GSDevTools.create();
