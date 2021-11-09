630522e1e8a2f5fb4e39571dc517593988a3e477
isabelgk24 <igk23@drexel.edu>
2021-11-08 22:43:23 -0500

    mostly done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,9 +12,11 @@ const mainTL = gsap.timeline();
 function appear(){
     const tl = gsap.timeline();
     tl.from("#Road",{duration: 0.5, y:"-=25", alpha:0})
-    tl.from("#Grassright",{duration: 0.5, alpha:0, morphSVG: "#grass-morph-right"})
+    tl.from("#Grassright",{duration: 0.5, alpha:0, morphSVG: "#grass-morph-right"}, "same1")
+    tl.from("#Grassleft",{duration: 0.5, alpha:0, morphSVG: "#grass-morph-left"}, "same1")
     tl.from(".mountains",{duration: 0.5, y:"+=50", alpha:0, stagger: -0.25})
-    tl.from(".lamp",{duration: 1, alpha:0, stagger: 0.25})
+    tl.from("#Sky",{duration: 0.5, alpha:0, morphSVG: "#sky-pre-morph"})
+    tl.from(".lamp",{duration: 1, alpha:0, stagger: 0.25}, "same")
     tl.from("#moon",{duration: 4.0, y:"+=150", alpha:0, rotation:90, transformOrigin: "50% 50%"}, "same")
     tl.from(".lightbulb",{duration: 2, alpha:0, stagger: .5},"same")
     tl.from(".glow",{duration: 2, alpha:0, stagger: .5}, "same")
@@ -23,99 +25,50 @@ function appear(){
     return tl;
 }
 
-// function mountainSlide(){
-//     const tl = gsap.timeline();
-//     tl.from(".mountains",{duration: 0.5, y:"+=50", alpha:0, stagger: -0.25});
-
-//     return tl;
-// }
-
-// function lampAppear(){
-//     const tl = gsap.timeline();
-//     tl.from(".lamp",{duration: 1.5, alpha:0, stagger: 0.5});
-
-//     return tl;
-// }
-
-// function moonRise(){
-//     const tl = gsap.timeline();
-//     tl.from("#moon",{duration: 4.0, y:"+=150", alpha:0, rotation:90, transformOrigin: "50% 50%"}, "same");
-
-//     return tl;
-// }
-
-// function lightAppear(){
-//     const tl = gsap.timeline();
-//     tl.from(".lightbulb",{duration: 2, alpha:0, stagger: .5},"same");
-
-//     return tl;
-// }
-
-// function glowAppear(){
-//     const tl = gsap.timeline();
-//     tl.from(".glow",{duration: 2, alpha:0, stagger: .5}, "same");
-
-//     return tl;
-// }
-
 function glowFlicker(){
     const tl = gsap.timeline();
 
-    const glowFlicker = gsap.timeline({repeat:10}, "same");
+    const glowFlicker = gsap.timeline({repeat: 2}, "same2");
     glowFlicker.from("#glow1",{duration:0.2,alpha:0})
-    .to("#glow1",{duration:0.1,alpha:0.7})
-    .to("#glow1",{duration:0.3,alpha:0.4})
-    .to("#glow1",{duration:0.1,alpha:0.6})
-    .to("#glow1",{duration:0.1,alpha:0.5})
-    .to("#glow1",{duration:0.2,alpha:0.6});
+    .to("#glow1",{duration:0.3,alpha:0.7})
+    .to("#glow1",{duration:0.4,alpha:0.6})
+    .to("#glow1",{duration:0.2,alpha:0.5})
+    .to("#glow1",{duration:0.3,alpha:0.6})
+    .to("#glow1",{duration:0.3,alpha:0.7});
 
-    const glowFlicker2 = gsap.timeline({repeat:10}, "same");
+    const glowFlicker2 = gsap.timeline({repeat: 2}, "same2");
     glowFlicker2.from("#glow2",{duration:0.2,alpha:0})
-    .to("#glow2",{duration:0.1,alpha:0.6})
-    .to("#glow2",{duration:0.1,alpha:0.5})
-    .to("#glow2",{duration:0.2,alpha:0.6})
-    .to("#glow2",{duration:0.1,alpha:0.7})
-    .to("#glow2",{duration:0.3,alpha:0.4});
+    .to("#glow2",{duration:0.4,alpha:0.6})
+    .to("#glow2",{duration:0.5,alpha:0.5})
+    .to("#glow2",{duration:0.3,alpha:0.6})
+    .to("#glow2",{duration:0.3,alpha:0.7});
 
-// const glowFlicker3 = gsap.timeline({repeat:10}, "same");
-// glowFlicker3.to("#glow2",{duration:0.2,alpha:0.5})
-// .to("#glow3",{duration:0.2,alpha:0.8})
-// .to("#glow3",{duration:0.1,alpha:0.4})
-// .to("#glow3",{duration:0.1,alpha:0.6})
-// .to("#glow3",{duration:0.2,alpha:0.9})
-// .to("#glow3",{duration:0.2,alpha:0.4});
+    const glowFlicker3 = gsap.timeline({repeat: 2}, "same2");
+    glowFlicker3.to("#glow2",{duration:0.2,alpha:0.5})
+    .to("#glow3",{duration:0.3,alpha:0.8})
+    .to("#glow3",{duration:0.4,alpha:0.6})
+    .to("#glow3",{duration:0.2,alpha:0.7})
+    .to("#glow3",{duration:0.2,alpha:0.9})
+    .to("#glow3",{duration:0.4,alpha:0.8});
 
-// const glowFlicker4 = gsap.timeline({repeat:10}, "same");
-// glowFlicker4.to("#glow4",{duration:0.2,alpha:0.5})
-// .to("#glow4",{duration:0.1,alpha:0.7})
-// .to("#glow4",{duration:0.3,alpha:0.4})
-// .to("#glow4",{duration:0.1,alpha:0.6})
-// .to("#glow4",{duration:0.1,alpha:0.5})
-// .to("#glow4",{duration:0.2,alpha:0.6});
+    const glowFlicker4 = gsap.timeline({repeat: 2}, "same2");
+    glowFlicker4.to("#glow4",{duration:0.2,alpha:0.5})
+    .to("#glow4",{duration:0.5,alpha:0.7})
+    .to("#glow4",{duration:0.3,alpha:0.8})
+    .to("#glow4",{duration:0.2,alpha:0.9})
+    .to("#glow4",{duration:0.3,alpha:0.8})
+    .to("#glow4",{duration:0.2,alpha:0.7});
 
-    tl.add(glowFlicker,"same")
-      .add(glowFlicker2,"same")
+    tl.add(glowFlicker,"same2")
+      .add(glowFlicker2,"same2")
+      .add(glowFlicker3,"same2")
+      .add(glowFlicker4,"same2")
+
 
     return tl;
 }
 
-
-
-
-// function starsAppear(){
-//     const tl = gsap.timeline();
-//     tl.from("#Stars",{duration: 3, y:"+=5", alpha:0, stagger: .1}, "same");
-
-//     return tl;
-// }
-
 mainTL.add(appear())
-        // .add(mountainSlide())
-        // .add(lampAppear())
-        // .add(moonRise(),"sameTime")
-        // .add(lightAppear(),"sameTime")
-        // .add(glowAppear(),"sameTime")
-        // .add(starsAppear(),"sameTime")
-        .add(glowFlicker(),"-=25%");
+        .add(glowFlicker(),"-=85%");
 
 GSDevTools.create();
