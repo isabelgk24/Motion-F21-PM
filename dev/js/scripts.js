fda3395fd19bfdafe4121f1674aff9b91702a295
isabelgk24 <igk23@drexel.edu>
2021-12-08 10:42:53 -0500

    finished project

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,13 +15,13 @@ const mainTL = gsap.timeline();
 
 function bounceIn(){
     const tl = gsap.timeline();
-    tl.to("#ball-1", {duration: 2.5, alpha:1, motionPath: {path: "#bounce-1", align: "#bounce-1", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"}, "same")
-    tl.to("#ball-2", {duration: 3, alpha:1, motionPath: {path: "#bounce-2", align: "#bounce-2", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same")
-    tl.to("#ball-3", {duration: 2.7, alpha:1, motionPath: {path: "#bounce-3", align: "#bounce-3", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same")
-    tl.to("#ball-4", {duration: 2.7, alpha:1, motionPath: {path: "#bounce-4", align: "#bounce-4", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same")
-    tl.to("#ball-5", {duration: 3, alpha:1, motionPath: {path: "#bounce-5", align: "#bounce-5", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same")
-    tl.to("#ball-6", {duration: 2.7, alpha:1, motionPath: {path: "#bounce-6", align: "#bounce-6", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same")
-    tl.to("#ball-7", {duration: 3, alpha:1, motionPath: {path: "#bounce-7", align: "#bounce-7", alignOrigin: [0.5, 0.5]}, ease: "power3.inOut"} , "same");
+    tl.to("#ball-1", {duration: 2.5, motionPath: {path: "#bounce-1", align: "#bounce-1", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"}, "same")
+    tl.to("#ball-2", {duration: 3, motionPath: {path: "#bounce-2", align: "#bounce-2", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same")
+    tl.to("#ball-3", {duration: 2.7, motionPath: {path: "#bounce-3", align: "#bounce-3", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same")
+    tl.to("#ball-4", {duration: 2.7, motionPath: {path: "#bounce-4", align: "#bounce-4", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same")
+    tl.to("#ball-5", {duration: 3, motionPath: {path: "#bounce-5", align: "#bounce-5", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same")
+    tl.to("#ball-6", {duration: 2.7, motionPath: {path: "#bounce-6", align: "#bounce-6", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same")
+    tl.to("#ball-7", {duration: 3, motionPath: {path: "#bounce-7", align: "#bounce-7", alignOrigin: [0.5, 0.5]}, ease: "power1.inOut"} , "same");
 
     return tl;
 
@@ -54,13 +54,23 @@ function barSlide(){
 
 function secondaryMoments(){
     const tl = gsap.timeline();
-    tl.to("#c-morph",{duration: .4, morphSVG:"#c"}, "same3")
+    tl.to("#c-morph", {duration: .4, morphSVG:"#c"}, "same3")
     tl.to("#g-morph",{duration: .4, morphSVG:"#g"}, "same3")
     tl.from("#bar-underline-draw",{duration: 1.5, drawSVG:0});
 
     return tl;
 
 }
+
+function ballFill(){
+    const tl = gsap.timeline();
+    tl.from(".ballfill",{duration: 1.5, y:"-=500", alpha:1, stagger:-.025, ease: "bounce.out"})
+    tl.from("#finance-2",{duration: 1, alpha:0})
+
+    return tl;
+
+}
+
 
 function slideOut(){
     const tl = gsap.timeline();
@@ -70,101 +80,21 @@ function slideOut(){
     tl.to("#ball-4",{duration: 1, y:"+=1200", alpha:0}, "same4")
     tl.to("#ball-5",{duration: 1.5, y:"-=1000", alpha:0}, "same4")
     tl.to("#ball-6",{duration: 1.2, y:"-=1100", alpha:0}, "same4")
-    tl.to("#ball-7",{duration: 1, y:"+=1200", alpha:0}, "same4");
+    tl.to("#ball-7",{duration: 1, y:"+=1200", alpha:0}, "same4")
+    tl.to(".ballfill", {duration: 1.2, x:"+=1000", alpha:0}, "same4")
+    tl.to("#finance-2", {duration: 1.2, x:"+=1000", alpha:0}, "same4")
+    tl.to("#bar-underline-draw",{duration: 1.7, x:"+=1000", alpha:0}, "same4");
 
     return tl;
-    
+
 }
 
-mainTL.add(bounceIn())
+mainTL
+        .add(bounceIn())
         .add(barMorph())
         .add(barSlide())
         .add(secondaryMoments(),"-=50%")
+        .add(ballFill())
         .add(slideOut());
 
 GSDevTools.create();
-
-// function appear(){
-//     const tl = gsap.timeline();
-//     tl.from("#road",{duration: 0.5, y:"-=25", alpha:0})
-//     tl.from("#grassright",{duration: 0.5, alpha:0, morphSVG: "#grass-morph-right"}, "same1")
-//     tl.from("#grassleft",{duration: 0.5, alpha:0, morphSVG: "#grass-morph-left"}, "same1")
-//     tl.from(".mountains",{duration: 0.5, y:"+=50", alpha:0, stagger: -0.25})
-//     tl.from("#sky",{duration: 0.5, alpha:0, morphSVG: "#sky-pre-morph"})
-//     tl.from(".lamp",{duration: 1, alpha:0, stagger: 0.25}, "same")
-//     tl.from("#moon",{duration: 4.0, y:"+=150", alpha:0, rotation:90, transformOrigin: "50% 50%"}, "same")
-//     tl.from(".lightbulb",{duration: 2, alpha:0, stagger: .5},"same")
-//     tl.from(".glow",{duration: 2, alpha:0, stagger: .5}, "same")
-//     tl.from("#stars",{duration: 3, y:"+=5", alpha:0, stagger: .1}, "same")
-//     tl.to("#preloader",{duration: .25, scale:0, alpha:0});
-
-//     return tl;
-// }
-
-// function glowFlicker(){
-//     const tl = gsap.timeline();
-
-//     const glowFlicker = gsap.timeline({repeat: 1},"same2");
-//     glowFlicker.from("#glow1",{duration:0,alpha:0})
-//     .to("#glow1",{duration:0.4,alpha:0.7})
-//     .to("#glow1",{duration:1.0,alpha:0.4})
-//     .to("#glow1",{duration:0.7,alpha:0.6})
-//     .to("#glow1",{duration:0.5,alpha:0.9})
-//     .to("#glow1",{duration:0.9,alpha:0.4})
-//     .to("#glow1",{duration:1.5,alpha:0.7});
-
-//     const glowFlicker2 = gsap.timeline({repeat: 1},"same2");
-//     glowFlicker2.from("#glow2",{duration:0,alpha:0})
-//     .to("#glow2",{duration:0.8,alpha:0.7})
-//     .to("#glow2",{duration:1.0,alpha:0.9})
-//     .to("#glow2",{duration:0.7,alpha:0.4})
-//     .to("#glow2",{duration:0.9,alpha:0.6})
-//     .to("#glow2",{duration:1.1,alpha:0.2});
-
-//     const glowFlicker3 = gsap.timeline({repeat: 1},"same2");
-//     glowFlicker3.from("#glow3",{duration:0,alpha:0})
-//     .to("#glow3",{duration:0.6,alpha:0.7})
-//     .to("#glow3",{duration:0.7,alpha:1.0})
-//     .to("#glow3",{duration:1.0,alpha:0.8})
-//     .to("#glow3",{duration:0.5,alpha:0.7})
-//     .to("#glow3",{duration:1.5,alpha:0.5})
-//     .to("#glow3",{duration:0.7,alpha:0.8});
-
-//     const glowFlicker4 = gsap.timeline({repeat: 1}, "same2");
-//     glowFlicker4.from("#glow4",{duration:0,alpha:0})
-//     .to("#glow4",{duration:1.5,alpha:0.7})
-//     .to("#glow4",{duration:0.6,alpha:0.4})
-//     .to("#glow4",{duration:1.0,alpha:0.6})
-//     .to("#glow4",{duration:0.4,alpha:0.9})
-//     .to("#glow4",{duration:0.9,alpha:0.6})
-//     .to("#glow4",{duration:0.6,alpha:0.7, onComplete: backToTop});
-
-//     tl.add(glowFlicker,"same2")
-//       .add(glowFlicker2,"same2")
-//       .add(glowFlicker3,"same2")
-//       .add(glowFlicker4,"same2")
-      
-//     return tl;
-// }
-
-// function backToTop (){
-//     window.scrollTo(0,0)
-//     gsap.set("#preloader", {display:"none"});
-// }
-
-// function heroAnimation (){
-//     const tl = gsap.timeline();
-
-//     const aniTime = 0.75;
-
-//     tl.from("#hero",{duration:1, alpha:0})
-//       .from("#hero h1", {duration:aniTime, y:"+=200", alpha:0})
-//       .from("#hero i", {duration:aniTime, y:"-=200", alpha:0, rotation:180})
-//       .from("#content1 ul li i",{duration:aniTime, y:"+=10", alpha:0})
-
-//     return tl;
-// }
-
-// mainTL.add(appear())
-//         .add(glowFlicker(),"-=47%")
-//         .add(heroAnimation(), "-=150%");
